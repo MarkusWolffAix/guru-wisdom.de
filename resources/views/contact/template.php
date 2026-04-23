5126062e174a6e50b1d3decaadd2bd82da6eb848
Markus Wolff <markus.wolff@guru-wisdom.com>
2026-04-23 19:46:57 +0200

    docs(contact): Update contact page text and signature
    Correct grammatical errors in the contact page description and change
    the tone from formal plural to personal singular. Add a personal
    sign-off to make the contact request more approachable.

--- resources/views/contact/template.php
+++ resources/views/contact/template.php
@@ -10,8 +10,15 @@ $this->setTitle('Kontakt');
 <div class="row mt-5">
     <div class="col-md-8 offset-md-2">
         <h1>Kontakt</h1>
-        <p>Wenn Sie ein eigene Weisheit mitteilein möchten und diese hier gern veröffentlichen möchten schreiben Sie unsg gerne eine Nachricht. 
-           Wir freuen uns über Ihre Nachricht und werden uns so schnell wie möglich bei Ihnen melden.</p>
+        <p>Wenn Sie eine eigene Weisheit mitteilein möchten und diese hier gern veröffentlichen würden, schreiben Sie mir gerne eine Nachricht. Ich freue mich über Ihre Nachricht und werde mich so schnell wie möglich bei Ihnen melden.
+<br/>
+<br/>
+
+        Herzlichst, 
+<br/>
+
+        Markus 
+</p>
         <span class="reverse-email">moc.modsiw-urug@ofni</span>
         </div>
 </div>
