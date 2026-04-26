ff7c04fb77d24a4db00e7d4f4a0d839f4f013126
Markus Wolff <markus.wolff@guru-wisdom.com>
2026-04-26 22:11:57 +0200

    chore(wisdom): Update S3 profile for Nuremberg endpoint
    Change the AWS profile used for the nbg1 object storage endpoint from
    'falkenstein' to 'nuernberg' to match updated configuration naming
    conventions.

--- bin/wisdomImageUpload.sh
+++ bin/wisdomImageUpload.sh
@@ -11,7 +11,7 @@ trap 'rm -rf "$TMP_DIR"' EXIT
 sips --resampleWidth 1280 -s format jpeg "$best_file" --out "$TMP_DIR/${ID}.jpg" >/dev/null 2>&1
 sips -s format jpeg -s formatOptions high "$best_file" --out "$TMP_DIR/${ID}_org.jpg" >/dev/null 2>&1
 
-aws s3 cp "$TMP_DIR/${ID}.jpg" s3://guru-wisdom-first/images/${ID}.jpg  --profile falkenstein --endpoint-url https://nbg1.your-objectstorage.com
-aws s3 cp "$TMP_DIR/${ID}_org.jpg" s3://guru-wisdom-first/images/org/${ID}.jpg  --profile falkenstein --endpoint-url https://nbg1.your-objectstorage.com
+aws s3 cp "$TMP_DIR/${ID}.jpg" s3://guru-wisdom-first/images/${ID}.jpg  --profile nuernberg --endpoint-url https://nbg1.your-objectstorage.com
+aws s3 cp "$TMP_DIR/${ID}_org.jpg" s3://guru-wisdom-first/images/org/${ID}.jpg  --profile nuernberg --endpoint-url https://nbg1.your-objectstorage.com
 aws s3 cp "$TMP_DIR/${ID}.jpg" s3://guru-wisdom-secound/images/${ID}.jpg  --profile helsinki --endpoint-url https://hel1.your-objectstorage.com
 aws s3 cp "$TMP_DIR/${ID}_org.jpg" s3://guru-wisdom-secound/images/org/${ID}.jpg  --profile helsinki --endpoint-url https://hel1.your-objectstorage.com
