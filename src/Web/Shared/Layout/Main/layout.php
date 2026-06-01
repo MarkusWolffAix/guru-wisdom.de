2f2cf43a2c9c7f1df731c986ae6da7b3640cc188
Markus Wolff <markus.wolff@guru-wisdom.com>
2026-06-01 20:59:13 +0200

    style(layout): Refactor social media link formatting
    Condense social media link HTML and remove the lazy loading attribute
    from footer icons. This improves code readability and ensures these
    small assets are loaded promptly with the rest of the layout.

--- src/Web/Shared/Layout/Main/layout.php
+++ src/Web/Shared/Layout/Main/layout.php
@@ -119,15 +119,9 @@ $this->beginPage();
         <div class="container text-center">
         <div class="mb-3">
            
-            <a href="https://www.instagram.com/guru2wisdom" class="me-3" >
-                <img src="/images/icons/instagram.png" alt="Instagram" width="20" height="20" loading="lazy">
-            </a>
-            <a href="https://www.facebook.com/profile.php?id=61577589013906" class="me-3">
-                <img src="/images/icons/facebook.png" alt="Facebook" width="20" height="20" loading="lazy">
-            </a>
-            <a href="https://www.youtube.com/@guru2wisdom" class="me-3">
-                <img src="/images/icons/youtube.png" alt="Youtube" width="20" height="20" loading="lazy">
-            </a>
+            <a href="https://www.instagram.com/guru2wisdom" class="me-3" ><img src="/images/icons/instagram.png" alt="Instagram" width="20" height="20" ></a>
+            <a href="https://www.facebook.com/profile.php?id=61577589013906" class="me-3"><img src="/images/icons/facebook.png" alt="Facebook" width="20" height="20" ></a>
+            <a href="https://www.youtube.com/@guru2wisdom" class="me-3"><img src="/images/icons/youtube.png" alt="Youtube" width="20" height="20" ></a>
         </div>
         
         <div class="small text-muted mb-2">
